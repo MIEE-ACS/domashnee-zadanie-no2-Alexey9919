1b65864f68fbbb0bf707e7f581d3d96a5a20c5a8
Alexey9919 <90642794+Alexey9919@users.noreply.github.com>
2021-10-16 20:14:28 +0400

    .

---
+++ bom1/bom1/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace bom1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("rnhgekrg" );
+            Console.WriteLine("Здарова ");
+        }
+    }
+}
---
+++ fibkjdofb/fibkjdofb/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace fibkjdofb
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
