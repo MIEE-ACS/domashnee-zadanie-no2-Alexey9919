b80f20c498a7915b94e9699dd4913d7f1d131289
Alexey9919 <90642794+Alexey9919@users.noreply.github.com>
2021-10-27 13:31:31 +0400

    /

--- DZ2_zagrebin_new/DZ2_zagrebin_new/Program.cs
+++ DZ2_zagrebin_new/DZ2_zagrebin_new/Program.cs
@@ -1,124 +1,182 @@
 ﻿using System;
 
 namespace ConsoleApp1
+
 {
+
     class Program
+
     {
 
-
         static double segment1(double x, double r)
+
         {
+
+            // На графике R = 2
+
             double a = -8;
-            double b = 0;
+
+            double b = -2;
+
             double y;
-            y = Math.Abs(Math.Sqrt(Math.Abs(r * r - (x - a) * (x - a) - b)));
+
+            y = -Math.Sqrt(r * r - (x - a) * (x - a)) - b;
+
             return y;
 
         }
+
         static double segment2()
+
         {
+
             double y;
+
             y = 2;
+
             return y;
-        }
-        static double segment3(double x)
-        {
-            double y;
-            y = -0.5 * x;
-            return y;
-        }
-        static double segment4(double x)
-        {
-            double y;
-            y = x - 3;
-            return y;
+
         }
 
-        static void value_finder(double R1, double R2, double i)
+        static double segment3(double x)
+
+        {
+
+            double y;
+
+            y = -0.5 * x;
+
+            return y;
+
+        }
+
+        static double segment4(double x)
+
+        {
+
+            double y;
+
+            y = x - 3;
+
+            return y;
+
+        }
+
+        static void value_finder(double i, double r)
+
         {
 
             if (i < -10)
+
             {
+
                 Console.WriteLine("Функция не определена");
+
             }
+
             else if (i < -6)
+
             {
-                if (R1 < 2)
-                {
-                    Console.WriteLine("При этих значениях возникают разрывы, выберите другой радиус: ");
-                }
-                else if (R1 > 2)
-                {
-                    Console.WriteLine("y({0:0.00}) = {1:0.00}", i, segment1(i, 0));
-                }
-                else if (R1 == 2)
-                {
-                    Console.WriteLine("y({0:0.00}) = {1:0.00}", i, segment1(i, 0));
-                }
+
+                if ((r < 2) && (Math.Abs(i + 8) >= r))
+
+                    Console.WriteLine("Точка разрыва второго рода");
+
+                else
+
+                    Console.WriteLine("y({0:0.00}) = {1:0.00}", i, segment1(i, r));
 
             }
 
             else if (i <= -4)
+
             {
+
                 Console.WriteLine("y({0:0.00}) = {1:0.00}", i, segment2());
+
             }
+
             else if (i <= 2)
+
             {
+
                 Console.WriteLine("y({0:0.00}) = {1:0.00}", i, segment3(i));
+
             }
+
             else if (i <= 4)
+
             {
+
                 Console.WriteLine("y({0:0.00}) = {1:0.00}", i, segment4(i));
+
             }
+
             else if (i > 4)
+
             {
+
                 Console.WriteLine("Функция не определена");
+
             }
+
         }
 
-
-
         static void Main(string[] args)
+
         {
 
-            double R1 = 0;
             double R = 0;
 
             Console.WriteLine("Введите радиус: ");
-            R1 = double.Parse(Console.ReadLine());
 
+            R = double.Parse(Console.ReadLine());
 
-            if (R1 < 0)
+            if (R < 0)
+
             {
-                while (R1 < 0)
+
+                while (R < 0)
+
                 {
 
                     Console.WriteLine("Радиус не может быть меньше 0, введите еще раз: ");
-                    R1 = double.Parse(Console.ReadLine());
+
+                    R = double.Parse(Console.ReadLine());
 
                 }
 
             }
-
-
 
             double in_value = 0;
 
             while (in_value != 100)
-            {
-                Console.WriteLine("Введите x (Если вы хотите посмотреть значения в виде таблицы, введите 100): ");
-                in_value = double.Parse(Console.ReadLine());
-                if (in_value != 100)
-                {
-                    value_finder(R1, R, in_value);
-                }
-            }
 
+            {
+
+                Console.WriteLine("Введите x (Если вы хотите посмотреть значения в виде таблицы, введите 100): ");
+
+                in_value = double.Parse(Console.ReadLine());
+
+                if (in_value != 100)
+
+                {
+
+                    value_finder(in_value, R);
+
+                }
+
+            }
 
             for (double x = -10; x <= 4; x = x + 0.2)
+
             {
-                value_finder(R1, R, x);
+
+                value_finder(x, R);
+
             }
+
         }
+
     }
 }
